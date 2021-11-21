a08c78bebd8415c20f7320cde2d5fecb6931798b
Umamaheswari <umavasanth27@gmail.com>
2021-11-21 19:54:03 +0530

    first commit

---
+++ Encapsulation/ApplyEncap.java
@@ -0,0 +1,58 @@
+package Encapsulation;
+
+/*Print college names(any 5 colleges) and departments - IT , CS , MECH , ECE , EEE , AERO , AUTO , EI - Should use private variables for the department name and college .
+ (Apply encapsulation)*/
+public class ApplyEncap {
+
+    private String colleageName1;
+    private String deptName1;
+    private String colleageName2;
+    private String deptName2;
+    private String colleageName3;
+    private String deptName3;
+    private String colleageName4;
+    private String deptName4;
+    private String colleageName5;
+    private String deptName5;
+
+
+    public void Display1(String cName1,String dName1){
+        colleageName1=cName1;
+        deptName1=dName1;
+        System.out.println("College Name:"+cName1);
+        System.out.println("Dept Name:"+deptName1);
+
+    }
+    public void Display2(String cName2,String dName2){
+        colleageName2=cName2;
+        deptName2=dName2;
+        System.out.println("College Name:"+cName2);
+        System.out.println("Dept Name:"+deptName2);
+
+    }
+    public void Display3(String cName3,String dName3){
+        colleageName3=cName3;
+        deptName3=dName3;
+        System.out.println("College Name:"+cName3);
+        System.out.println("Dept Name:"+deptName3);
+
+    }
+    public void Display4(String cName4,String dName4){
+        colleageName4=cName4;
+        deptName4=dName4;
+        System.out.println("College Name:"+cName4);
+        System.out.println("Dept Name:"+deptName4);
+
+    }
+    public void Display5(String cName5,String dName5){
+        colleageName5=cName5;
+        deptName5=dName5;
+        System.out.println("College Name:"+cName5);
+        System.out.println("Dept Name:"+deptName5);
+
+    }
+
+
+
+
+}
---
+++ Encapsulation/EncapTest.java
@@ -0,0 +1,25 @@
+package Encapsulation;
+
+public class EncapTest extends ApplyEncap{
+
+    public static void main(String[] args) {
+        EncapTest encapTest=new EncapTest();
+        encapTest.Display1("Sai Ram college","IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+        encapTest.Display2("Shastra University","IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+        encapTest.Display3("Apollo College","IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+        encapTest.Display4("Meenakshi College","IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+        encapTest.Display5("Periyar Maniammai","IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
