9e794dec1605a0900760a33c612bfc1e918176b0
Umamaheswari <umavasanth27@gmail.com>
2021-11-29 19:10:25 +0530

    changestoEncapsulationProgram

--- Encapsulation/EncapTest.java
+++ Encapsulation/EncapTest.java
@@ -1,25 +1,60 @@
 package Encapsulation;
 
-public class EncapTest extends ApplyEncap{
+public class EncapTest{
 
     public static void main(String[] args) {
-        EncapTest encapTest=new EncapTest();
-        encapTest.Display1("Sai Ram college","IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
-        encapTest.Display2("Shastra University","IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
-        encapTest.Display3("Apollo College","IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
-        encapTest.Display4("Meenakshi College","IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
-        encapTest.Display5("Periyar Maniammai","IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+        ApplyEncap encap=new ApplyEncap();
+        encap.setCollegeName("Sai");
+        encap.setDeptName("IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
 
+        System.out.println("College Name:"+encap.getCollegeName()+ "Departments "+encap.getDeptName());
+        ApplyEncap encap2=new ApplyEncap();
+        encap2.setCollegeName("Shastra");
+        encap2.setDeptName("IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+        System.out.println("College Name:"+encap2.getCollegeName()+ "Departments "+encap2.getDeptName());
 
+        ApplyEncap encap3=new ApplyEncap();
+        encap3.setCollegeName("Apollo");
+        encap3.setDeptName("IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+        System.out.println("College Name:"+encap3.getCollegeName()+ "Departments "+encap3.getDeptName());
 
-
-
-
-
+        ApplyEncap encap4=new ApplyEncap();
+        encap4.setCollegeName("Venkateswara");
+        encap4.setDeptName("IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+        System.out.println("College Name:"+encap4.getCollegeName()+ "Departments "+encap4.getDeptName());
+        ApplyEncap encap5=new ApplyEncap();
+        encap5.setCollegeName("Periyar");
+        encap5.setDeptName("IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+        System.out.println("College Name:"+encap5.getCollegeName()+ "Departments "+encap5.getDeptName());
 
 
 
 
 
     }
+
 }
+
+    /*public void printDetails() {
+        EncapTest en1= new EncapTest();
+
+        en1.setCollegeName("Sai Ram");
+        en1.setDeptName("IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+
+        EncapTest en2= new EncapTest();
+
+        en2.setCollegeName("Shastra");
+        en2.setDeptName("IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+
+        EncapTest en3= new EncapTest();
+        en3.setCollegeName("Apollo");
+        en3.setDeptName("IT , CS , MECH , ECE , EEE , AERO ,AUTO,EI");
+        System.out.println();
+
+    }
+
+
+    }
+*/
+
+
